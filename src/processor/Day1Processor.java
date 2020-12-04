82e39caae21a5acdbc53fd56c7e1180191486569
Michelle <michellekoob828@gmail.com>
2020-12-03 22:06:34 -0600

    day 1 pt 2

--- src/processor/Day1Processor.java
+++ src/processor/Day1Processor.java
@@ -12,6 +12,7 @@ public class Day1Processor {
 	public static final String FILE_NAME = "src/resources/InputDay1";
 
 	public void process() throws FileNotFoundException {
+		System.out.println("Part 1 Started");
 		List<Integer> expenseReportEntries = this.mapExpenseReportEntries();
 		boolean foundEntries = false;
 		
@@ -33,6 +34,44 @@ public class Day1Processor {
 				break;
 			}
 		}
+		System.out.println("Part 1 Ended");
+	}
+	
+	public void processPart2() {
+		System.out.println("Part 2 Started");
+		List<Integer> expenseReportEntries = this.mapExpenseReportEntries();
+		boolean foundEntries = false;
+		
+		for(int i = 0; i < expenseReportEntries.size(); i++) {
+			Integer int1 = expenseReportEntries.get(i);
+			
+			for(int j = i+1; j < expenseReportEntries.size(); j++) {
+				Integer int2 = expenseReportEntries.get(j);
+				
+				for(int k = j+1; k < expenseReportEntries.size(); k++) {
+					Integer int3 = expenseReportEntries.get(k);
+					if(2020 == int1 + int2 + int3) {
+						System.out.println("int1 = " + int1);
+						System.out.println("int2 = " + int2);
+						System.out.println("int3 = " + int3);
+						System.out.println("int1 + int2 + int3 = " + (int1 + int2 + int3));
+						System.out.println("int1 * int2 * int3 = " + (int1 * int2 * int3));
+						foundEntries = true;
+						break;
+					}
+					if(foundEntries) {
+						break;
+					}
+				}
+				if(foundEntries) {
+					break;
+				}
+			}
+			if(foundEntries) {
+				break;
+			}
+		}
+		System.out.println("Part 2 Ended");
 	}
 	
 	private List<Integer> mapExpenseReportEntries() {
